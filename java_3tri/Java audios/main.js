271eafcbf79543f2b9b1d26af166c65a48774bb1
Maria Eduarda <105428723+0nstar25@users.noreply.github.com>
2023-11-08 11:38:41 -0300

    Update main.js

--- java_3tri/Java audios/main.js
+++ java_3tri/Java audios/main.js
@@ -1,1 +1,17 @@
-
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listadeteclas=document.querySelectorAll('.tecla');
+let contador=0
+while(contador <listadeteclas.lenght) {
+const efeito= listadeteclas[contador]. classlist[1];
+const idAudio= "#som_"+efeito;
+listadeteclas[contador].onclick=function(){
+    tocaSom(idAudio);
+}
+contador=contador +1 ;
+console.log(contador);
+}
+tecla.onkeydown=function(){
+    tecla.add('ativa')
+}
